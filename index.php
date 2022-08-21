402d5afe6aa3a9015c00aa80d551a4b0577295f5
Ravo1405 <ravonirina.rasolofomanana@yahoo.com>
2022-08-21 18:57:46 +0200

    ajout du fichier index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix.  Attention age est de type entier.  Afficher leur contenu
+//Création des varibles lastname, firstname, et age et les initialiser avec une chaine de caractère et entier.
+    $lastname = "Pitt";
+    $firstname = "Brad";
+    $age = 50;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les variables exercice 2</title>
+</head>
+<body>
+    <!--Affichage du contenu des 3 variables dans une balise HTML p via un echo PHP. -->
+    <p><?php echo "Je m'appelle $firstname $lastname et j'ai $age ans."; ?></p>
+</body>
+</html>
+
+
